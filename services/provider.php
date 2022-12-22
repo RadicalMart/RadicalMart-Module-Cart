79363e079e6e3039cf40ee7c5de9c370ce0bd2a5
Igor Berdichevskiy <iberdichevskiy@septdir.com>
2022-12-22 13:13:21 +0300

    Prepare 1.1.0 Release
    * Refactoring

---
+++ services/provider.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ * @package     RadicalMart Cart Module
+ * @subpackage  mod_radicalmart_cart
+ * @version     1.1.0
+ * @author      Delo Design - delo-design.ru
+ * @copyright   Copyright (c) 2022 Delo Design. All rights reserved.
+ * @license     GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
+ * @link        https://delo-design.ru/
+ */
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Extension\Service\Provider\HelperFactory;
+use Joomla\CMS\Extension\Service\Provider\Module;
+use Joomla\CMS\Extension\Service\Provider\ModuleDispatcherFactory;
+use Joomla\DI\Container;
+use Joomla\DI\ServiceProviderInterface;
+
+return new class implements ServiceProviderInterface {
+
+	/**
+	 * Registers the service provider with a DI container.
+	 *
+	 * @param   Container  $container  The DI container.
+	 *
+	 * @return  void
+	 *
+	 * @since   1.1.0
+	 */
+	public function register(Container $container)
+	{
+		$container->registerServiceProvider(new ModuleDispatcherFactory('\\Joomla\\Module\\RadicalMartCart'));
+		$container->registerServiceProvider(new HelperFactory('\\Joomla\\Module\\RadicalMartCart\\Site\\Helper'));
+
+		$container->registerServiceProvider(new Module());
+	}
+};
--- tmpl/default.php
+++ tmpl/default.php
@@ -1,28 +1,14 @@
 <?php
 /*
- * @package     RadicalMart Package
+ * @package     RadicalMart Cart Module
  * @subpackage  mod_radicalmart_cart
- * @version     1.0.0
+ * @version     1.1.0
  * @author      Delo Design - delo-design.ru
- * @copyright   Copyright (c) 2021 Delo Design. All rights reserved.
+ * @copyright   Copyright (c) 2022 Delo Design. All rights reserved.
  * @license     GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
  * @link        https://delo-design.ru/
  */
 
 defined('_JEXEC') or die;
 
-use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Layout\LayoutHelper;
-
-HTMLHelper::script('com_radicalmart/cart.min.js', array('version' => 'auto', 'relative' => true));
-HTMLHelper::script('com_radicalmart/axios.min.js', array('version' => 'auto', 'relative' => true));
-if ($componentParams->get('radicalmart_js', 1))
-{
-	HTMLHelper::script('com_radicalmart/radicalmart.min.js', array('version' => 'auto', 'relative' => true));
-}
-if ($componentParams->get('trigger_js', 1))
-{
-	HTMLHelper::script('com_radicalmart/trigger.min.js', array('version' => 'auto', 'relative' => true));
-}
-
-echo LayoutHelper::render('components.radicalmart.cart.icon');
+echo \Joomla\CMS\Layout\LayoutHelper::render('components.radicalmart.cart.icon');
